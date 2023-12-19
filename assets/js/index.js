cddf2f5da03616a91a0063cc30cd42db4bac85c6
eshwar-ishq <eshwarchandra334@gmail.com>
2023-12-19 12:19:18 +0530

    updated saperate filtering function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 
 let searchBar = document.getElementById('myInput');
-let responseData = [];
 
 const prevButton = document.getElementById('prev_Button')
 const nextButton = document.getElementById('next_Button')
@@ -8,9 +7,9 @@ let currentPage = 1;
 
 
 const updatePageContent = () => {
-	fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Belmont?page=${currentPage}&limit=30`)
+	// fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Belmont?page=${currentPage}&limit=30`)
 	// fetch(`http://localhost:5000/pmsstock/19?page=${currentPage}&limit=20`)
-	// fetch(`http://localhost:5000/combinedstocks/Belmont?page=${currentPage}&limit=30`)
+	fetch(`http://localhost:5000/combinedstocks/Belmont?page=${currentPage}&limit=30`)
 		.then((respond) => {
 			console.log(respond.status)
 			return respond.json();
@@ -30,18 +29,18 @@ const updatePageContent = () => {
 							<td>${e.Purchase_amt}</td>
 							<td>${e.Item_Amount}</td>
 							<td>${e.Batch}</td>
-							<td>${e.Date_modified}</td>
 							<td>${e.Available_Qty}</td>
 							<td>${e.Blocked_Qty}</td>
 							<td>${e.Sheet_name}</td>
+							<td>${e.Date_modified}</td>
 						</tr>`
 			})
 			document.getElementById("belmont_table_body").innerHTML = storedData
 		})
 
-	fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Richmond?page=${currentPage}&limit=30`)
+	// fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Richmond?page=${currentPage}&limit=30`)
 	// fetch(`http://localhost:5000/pmsstock/20?page=${currentPage}&limit=20`)
-	// fetch(`http://localhost:5000/combinedstocks/Richmond?page=${currentPage}&limit=30`)
+	fetch(`http://localhost:5000/combinedstocks/Richmond?page=${currentPage}&limit=30`)
 		.then((respond) => {
 			console.log(respond.status)
 			return respond.json();
@@ -61,18 +60,18 @@ const updatePageContent = () => {
 							<td>${e.Purchase_amt}</td>
 							<td>${e.Item_Amount}</td>
 							<td>${e.Batch}</td>
-							<td>${e.Date_modified}</td>
 							<td>${e.Available_Qty}</td>
 							<td>${e.Blocked_Qty}</td>
 							<td>${e.Sheet_name}</td>
+							<td>${e.Date_modified}</td>
 						</tr>`
 			})
 			document.getElementById('richmond_table_body').innerHTML = storedData;
 		})
 
-	fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Hyderabad?page=${currentPage}&limit=30`)
+	// fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Hyderabad?page=${currentPage}&limit=30`)
 	// fetch(`http://localhost:5000/pmsstock/1?page=${currentPage}&limit=20`)
-	// fetch(`http://localhost:5000/combinedstocks/Hyderabad?page=${currentPage}&limit=30`)
+	fetch(`http://localhost:5000/combinedstocks/Hyderabad?page=${currentPage}&limit=30`)
 		.then((respond) => {
 			console.log(respond.status)
 			return respond.json();
@@ -92,18 +91,18 @@ const updatePageContent = () => {
 							<td>${e.Purchase_amt}</td>
 							<td>${e.Item_Amount}</td>
 							<td>${e.Batch}</td>
-							<td>${e.Date_modified}</td>
 							<td>${e.Available_Qty}</td>
 							<td>${e.Blocked_Qty}</td>
 							<td>${e.Sheet_name}</td>
+							<td>${e.Date_modified}</td>
 						</tr>`
 			})
 			document.getElementById('hyderabad_table_body').innerHTML = storedData;
 		})
 
-	fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Haridwar?page=${currentPage}&limit=30`)
+	// fetch(`https://my-server-u7xs.onrender.com/combinedstocks/Haridwar?page=${currentPage}&limit=30`)
 	// fetch(`http://localhost:5000/pmsstock/17?page=${currentPage}&limit=20`)
-	// fetch(`http://localhost:5000/combinedstocks/Haridwar?page=${currentPage}&limit=30`)
+	fetch(`http://localhost:5000/combinedstocks/Haridwar?page=${currentPage}&limit=30`)
 		.then((respond) => {
 			console.log(respond.status)
 			return respond.json();
@@ -123,10 +122,10 @@ const updatePageContent = () => {
 							<td>${e.Purchase_amt}</td>
 							<td>${e.Item_Amount}</td>
 							<td>${e.Batch}</td>
-							<td>${e.Date_modified}</td>
 							<td>${e.Available_Qty}</td>
 							<td>${e.Blocked_Qty}</td>
 							<td>${e.Sheet_name}</td>
+							<td>${e.Date_modified}</td>
 						</tr>`
 			})
 			document.getElementById('hariwdar_table_body').innerHTML = storedData;
@@ -136,45 +135,45 @@ const updatePageContent = () => {
 updatePageContent();
 
 // const searchBarFunction = () => {
-// 	let belmontData, richmondData, allData = [];
-// 	const itemsPerPage = 30;
+// 	let allData = [];
+// 	const itemsPerPage = 40;
 // 	let currentPage = 1;
-// 	let belmontId = document.getElementById('belmont_table_body');
-// 	let richmondId = document.getElementById('richmond_table_body');
-// 	let allUpdatedIds = [belmontId, richmondId];
 
 // 	// Fetch All data initially
 // 	fetch('http://localhost:5000/allpmcstocks')
+// 	// fetch('https://my-server-u7xs.onrender.com/allpmcstocks')
+// 	// fetch(`https://my-server-u7xs.onrender.com/allpmcstocks`)
 // 		.then((response) => response.json())
 // 		.then((response) => {
 // 			allData = response;
-// 			// Fetch Belmont data after fetching allData
-// 			return fetch(`http://localhost:5000/pmsstock/19?page=${currentPage}&limit=50`);
-// 		})
-// 		.then((res) => res.json())
-// 		.then((response) => {
-// 			belmontData = response;
-// 			// Fetch Richmond data after fetching BelmontData
-// 			return fetch(`http://localhost:5000/pmsstock/20?page=${currentPage}&limit=50`);
-// 		})
-// 		.then((res) => res.json())
-// 		.then((response) => {
-// 			richmondData = response;
 
 // 			// Attach input event listener after all data has been fetched
 // 			document.getElementById('myInput').addEventListener('input', function () {
 // 				let searchValue = this.value.toLowerCase();
 
-// 				// Filter allData based on the searchValue
-// 				let AllFilteredData = allData.filter((e) => {
-// 					return (
-// 						e.Item_name && e.Item_name.toLowerCase().includes(searchValue) ||
-// 						e.MSKU && e.MSKU.toLowerCase().includes(searchValue)
-// 					);
-// 				});
+// 				// Get the selected category
+// 				const selectedCategory = document.getElementById('category').value;
+
+// 				let allFilteredData = allData.filter((e) => {
+// 					let fieldvalue = e[selectedCategory]
+
+// 					if(fieldvalue !== undefined && (typeof fieldvalue === "string" || typeof fieldvalue === "number" )) {
+// 						// const fieldValeString = typeof fieldvalue === 'string' ? fieldvalue.toLowerCase() : String(fieldvalue);
+// 						// return fieldValeString === searchValue;
+// 						return String(fieldvalue).toLowerCase().includes(searchValue);	
+// 					}
+
+// 					return false;
+// 				})
+
+// 				// // Filter allData based on the selectedCategory and searchValue
+// 				// let allFilteredData = allData.filter((e) => {
+// 				// 	return e[selectedCategory] && e[selectedCategory].toLowerCase().includes(searchValue) 
+// 				// });
 
 // 				// Update all tables with the filtered and paginated data
-// 				updateTables(getPageData(AllFilteredData, currentPage, itemsPerPage));
+// 				// updateTables(getPageData(allFilteredData, currentPage, itemsPerPage));
+// 				updateTables(allFilteredData);
 // 			});
 // 		})
 // 		.catch((err) => {
@@ -183,32 +182,32 @@ updatePageContent();
 // };
 
 // const updateTables = (data) => {
-// 	const tableIds = ['belmont_table_body', 'richmond_table_body', 'hariwdar_table_body', 'hyderabad_table_body' ];
+// 	const tableIds = ['belmont_table_body', 'richmond_table_body', 'hariwdar_table_body', 'hyderabad_table_body'];
 
 // 	tableIds.forEach((tableId) => {
 // 		let storedFullData = '';
 // 		data.forEach((e) => {
 // 			storedFullData += `<tr key=${e.id}>
-//                                 <td>${e.Date_modified}</td>
-//                                 <td>${e.ID}</td>
-//                              	<td>${e.MSKU}</td>
-//                              	<td>${e.Item_name}</td>
-//                              	<td>${e.Quantity}</td>
-//                              	<td>${e.Location}</td>
-//                              	<td>${e.Brand}</td>
-//                              	<td>${e.Product_Category}</td>
-//                              	<td>${e.Category}</td>
-//                              	<td>${e.MRP}</td>
-//                              	<td>${e.Item_Amount}</td>
-//                              	<td>${e.Batch}</td>
-//                              	<td>${e.Manf_date}</td>
-//                              	<td>${e.Expiry_date}</td>
-//                              	<td>${e.Available_Qty}</td>
-//                             	<td>${e.Blocked_Qty}</td>
-//                             </tr>`;
+// 									<td>${e.id}</td>
+// 									<td>${e.MSKU}</td>
+// 									<td>${e.Item_name}</td>
+// 									<td>${e.Quantity}</td>
+// 									<td>${e.LocationName}</td>
+// 									<td>${e.Brand}</td>
+// 									<td>${e.Product_Category}</td>
+// 									<td>${e.Category}</td>
+// 									<td>${e.Purchase_amt}</td>
+// 									<td>${e.Item_Amount}</td>
+// 									<td>${e.Batch}</td>
+// 									<td>${e.Date_modified}</td>
+// 									<td>${e.Available_Qty}</td>
+// 									<td>${e.Blocked_Qty}</td>
+// 									<td>${e.Sheet_name}</td>
+// 						  		</tr>`;
 // 		});
 
 // 		const tableElement = document.getElementById(tableId);
+// 		console.log(tableElement);
 
 // 		if (tableElement) {
 // 			tableElement.innerHTML = storedFullData;
@@ -219,7 +218,6 @@ updatePageContent();
 // 	});
 // };
 
-
 // // Function to get a specific page of data
 // const getPageData = (data, currentPage, itemsPerPage) => {
 // 	const startIndex = (currentPage - 1) * itemsPerPage;
@@ -227,105 +225,93 @@ updatePageContent();
 // 	return data.slice(startIndex, endIndex);
 // };
 
-// // Call the function to initiate the process
+// // Initial function call
 // searchBarFunction();
 
-const searchBarFunction = () => {
-	let allData = [];
-	const itemsPerPage = 40;
-	let currentPage = 1;
+//==============================================================================
 
-	// Fetch All data initially
-	// fetch('http://localhost:5000/allpmcstocks')
-	fetch('https://my-server-u7xs.onrender.com/allpmcstocks')
-	// fetch(`https://my-server-u7xs.onrender.com/allpmcstocks`)
-		.then((response) => response.json())
-		.then((response) => {
-			allData = response;
+const searchBarFunction = (tableId) => {
+    // let tableData = [];
+    // const itemsPerPage = 40;
+    // let currentPage = 1;
 
-			// Attach input event listener after all data has been fetched
-			document.getElementById('myInput').addEventListener('input', function () {
-				let searchValue = this.value.toLowerCase();
+    // Fetch data for the specific table
+    // fetch(`http://localhost:5000/combinedstocks/Belmont?${tableId}?page=${currentPage}&limit=30`)
+	fetch(`http://localhost:5000/allpmcstocks/Belmont?`)
+        .then((response) => response.json())
+        .then((response) => {
+            tableData = response;
+			console.log(tableData)
 
-				// Get the selected category
-				const selectedCategory = document.getElementById('category').value;
+            // Attach input event listener after the data has been fetched
+            document.getElementById('myInput').addEventListener('input', function () {
+                let searchValue = this.value.toLowerCase();
 
-				let allFilteredData = allData.filter((e) => {
-					let fieldvalue = e[selectedCategory]
+                // Get the selected category
+                const selectedCategory = document.getElementById('category').value;
 
-					if(fieldvalue !== undefined && (typeof fieldvalue === "string" || typeof fieldvalue === "number" )) {
-						// const fieldValeString = typeof fieldvalue === 'string' ? fieldvalue.toLowerCase() : String(fieldvalue);
-						// return fieldValeString === searchValue;
-						return String(fieldvalue).toLowerCase().includes(searchValue);	
-					}
+                let tableFilteredData = tableData.filter((e) => {
+                    let fieldValue = e[selectedCategory];
 
-					return false;
-				})
+                    if (fieldValue !== undefined && (typeof fieldValue === "string" || typeof fieldValue === "number")) {
+                        return String(fieldValue).toLowerCase().includes(searchValue);
+                    }
 
-				// // Filter allData based on the selectedCategory and searchValue
-				// let allFilteredData = allData.filter((e) => {
-				// 	return e[selectedCategory] && e[selectedCategory].toLowerCase().includes(searchValue) 
-				// });
+                    return false;
+                });
 
-				// Update all tables with the filtered and paginated data
-				updateTables(getPageData(allFilteredData, currentPage, itemsPerPage));
-			});
-		})
-		.catch((err) => {
-			console.log('Error fetching data:', err);
-		});
+                // Update the specific table with the filtered and paginated data
+                updateTable(tableId, tableFilteredData);
+            });
+        })
+        .catch((err) => {
+            console.log('Error fetching data:', err);
+        });
 };
 
-const updateTables = (data) => {
-	const tableIds = ['belmont_table_body', 'richmond_table_body', 'hariwdar_table_body', 'hyderabad_table_body'];
+const updateTable = (tableId, data) => {
+    let storedFullData = '';
+    data.forEach((e) => {
+        storedFullData += `<tr key=${e.id}>
+                                <td>${e.id}</td>
+                                <td>${e.MSKU}</td>
+                                <td>${e.Item_name}</td>
+                                <td>${e.Quantity}</td>
+                                <td>${e.LocationName}</td>
+                                <td>${e.Brand}</td>
+                                <td>${e.Product_Category}</td>
+                                <td>${e.Category}</td>
+                                <td>${e.Purchase_amt}</td>
+                                <td>${e.Item_Amount}</td>
+                                <td>${e.Batch}</td>
+                                <td>${e.Available_Qty}</td>
+                                <td>${e.Blocked_Qty}</td>
+                                <td>${e.Sheet_name}</td>
+                                <td>${e.Date_modified}</td>
+                            </tr>`;
+    });
 
-	tableIds.forEach((tableId) => {
-		let storedFullData = '';
-		data.forEach((e) => {
-			storedFullData += `<tr key=${e.id}>
-									<td>${e.id}</td>
-									<td>${e.MSKU}</td>
-									<td>${e.Item_name}</td>
-									<td>${e.Quantity}</td>
-									<td>${e.LocationName}</td>
-									<td>${e.Brand}</td>
-									<td>${e.Product_Category}</td>
-									<td>${e.Category}</td>
-									<td>${e.Purchase_amt}</td>
-									<td>${e.Item_Amount}</td>
-									<td>${e.Batch}</td>
-									<td>${e.Date_modified}</td>
-									<td>${e.Available_Qty}</td>
-									<td>${e.Blocked_Qty}</td>
-									<td>${e.Sheet_name}</td>
-						  		</tr>`;
-		});
+    const tableElement = document.getElementById(tableId);
+    console.log(tableElement);
 
-		const tableElement = document.getElementById(tableId);
-		console.log(tableElement);
-
-		if (tableElement) {
-			tableElement.innerHTML = storedFullData;
-			console.log(tableElement.innerHTML);
-		} else {
-			console.error("Table element not found with ID:", tableId);
-		}
-	});
+    if (tableElement) {
+        tableElement.innerHTML = storedFullData;
+        console.log(tableElement.innerHTML);
+    } else {
+        console.error("Table element not found with ID:", tableId);
+    }
 };
 
-// Function to get a specific page of data
-const getPageData = (data, currentPage, itemsPerPage) => {
-	const startIndex = (currentPage - 1) * itemsPerPage;
-	const endIndex = startIndex + itemsPerPage;
-	return data.slice(startIndex, endIndex);
-};
+// Call the function to initiate the process for each table
+searchBarFunction('belmont_table_body');
+searchBarFunction('richmond_table_body');
+searchBarFunction('hyderabad_table_body');
+searchBarFunction('hariwdar_table_body');
 
-// Initial function call
-searchBarFunction();
 
 const prevButtonFun = () => {
 	currentPage--;
-	updatePageContent()
+	updatePageContent()  
 }
 
 const nextButtonFun = () => {
